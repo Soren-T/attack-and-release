97775820405132b01dcb10b0f06bd92dd832f56d
Soren Thompson <sorenct04@gmail.com>
2020-02-27 18:29:10 -0700

    initial commit

---
+++ src/components/Main.js
@@ -0,0 +1,52 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import Recordings from './Pages/Recordings';
+import Podcast from './Pages/Podcast';
+import About from './Pages/About';
+import Schedule from './Pages/Schedule';
+import Services from './Pages/Services';
+
+class Main extends React.Component {
+  render() {
+    let close = (
+      <div
+        className="close"
+        onClick={() => {
+          this.props.onCloseArticle()
+        }}
+      />
+    )
+
+    const { articleTimeout, article, setWrapperRef, timeout } = this.props;
+
+    return (
+      <div
+        ref={setWrapperRef}
+        id="main"
+        style={timeout ? { display: 'flex' } : { display: 'none' }}
+      >
+        <Recordings close={close} article={article} timeout={articleTimeout} />
+
+        <Podcast close={close} article={article} timeout={articleTimeout} />
+
+        <About close={close} article={article} timeout={articleTimeout} />
+
+        <Schedule close={close} article={article} timeout={articleTimeout} />
+
+        <Services close={close} article={article} timeout={articleTimeout} />
+
+      </div>
+    )
+  }
+}
+
+Main.propTypes = {
+  route: PropTypes.object,
+  article: PropTypes.string,
+  articleTimeout: PropTypes.bool,
+  onCloseArticle: PropTypes.func,
+  timeout: PropTypes.bool,
+  setWrapperRef: PropTypes.func.isRequired,
+}
+
+export default Main
---
+++ src/components/Pages/About.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import pic03 from '../../images/pic03.jpg'
+
+const About = ({ close, article, timeout }) => (
+  <article
+    id="about"
+    className={`${article === 'about' ? 'active' : ''} ${
+      timeout ? 'timeout' : ''
+    }`}
+    style={{ display: 'none' }}
+  >
+    <h2 className="major">About</h2>
+    <span className="image main">
+      <img src={pic03} alt="" />
+    </span>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur et adipiscing elit. Praesent
+      eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam
+      erat volutpat. Praesent urna nisi, fringila lorem et vehicula
+      lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices.
+      Aliquam libero et malesuada fames ac ante ipsum primis in faucibus.
+      Cras viverra ligula sit amet ex mollis mattis lorem ipsum dolor sit
+      amet.
+    </p>
+    {close}
+  </article>
+)
+
+export default About

7f22b7e815f84bea89b678ef523bb1060482c34c
Soren Thompson <sorenct04@gmail.com>
2020-04-14 15:15:23 -0600

    fix build error: window not defined

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,19 +1,29 @@
 import { useState, useEffect } from 'react';
 
+const windowExists = typeof window !== `undefined`;
+
 const useWindowSize = () => {
-    const [width, setWidth] = useState(window.innerWidth);
-    const [height, setHeight] = useState(window.innerHeight);
-    const handleWindowResize = () => {
-      setWidth(window.innerWidth);
-      setHeight(window.innerHeight);
-    };
-  
-    useEffect(() => {
+  let h = 0;
+  let w = 0;
+  if (windowExists) {
+    const { innerHeigth, innerWidth } = window;
+    [ h, w ] = [ innerHeigth, innerWidth];
+  }
+  const [width, setWidth] = useState(w);
+  const [height, setHeight] = useState(h);
+  const handleWindowResize = () => {
+    setWidth(window.innerWidth);
+    setHeight(window.innerHeight);
+  };
+
+  useEffect(() => {
+    if(windowExists) {
       window.addEventListener("resize", handleWindowResize);
       return () => window.removeEventListener("resize", handleWindowResize);
-    }, []);
-  
-    return { width, height };
+    }
+  }, []);
+
+  return { width, height };
 };
 
 export default useWindowSize;
